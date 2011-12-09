7d1f8b4f963c4080a4f8923951e24e5b65740117
Achilles Charmpilas <ac@humbuckercode.co.uk>
2011-12-09 17:11:04 +0100

    posts controller now supports simple authentication via the 'author' and 'user_password' query values

--- controllers/posts.php
+++ controllers/posts.php
@@ -8,9 +8,7 @@ class JSON_API_Posts_Controller {
 
   public function create_post() {
     global $json_api;
-    if (!current_user_can('edit_posts')) {
-      $json_api->error("You need to login with a user capable of creating posts.");
-    }
+    $this->authenticate();
     if (!$json_api->query->nonce) {
       $json_api->error("You must include a 'nonce' value to create posts. Use the `get_nonce` Core API method.");
     }
@@ -29,6 +27,29 @@ class JSON_API_Posts_Controller {
     );
   }
   
+  /**
+   * Attempts to authenticate user if author and user_password fields exist
+   *
+   * @return void
+   * @author Achillefs Charmpilas
+   */
+  private function authenticate() {
+    global $json_api;
+    if ($json_api->query->author && $json_api->query->user_password) {
+      $user = wp_signon(array('user_login' => $json_api->query->author, 'user_password' => $json_api->query->user_password));
+      if (get_class($user) == 'WP_Error') {
+        $json_api->error($user->errors);
+      } else {
+        if (!user_can($user->ID,'edit_posts')) {
+          $json_api->error("You need to login with a user capable of creating posts.");
+        }
+      }
+    } else {
+      if (!current_user_can('edit_posts')) {
+        $json_api->error("You need to login with a user capable of creating posts.");
+      }
+    }
+  }
 }
 
 ?>
