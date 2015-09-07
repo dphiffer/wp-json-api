5367a424b7ab4a5149a817bb0e14ac921d8b3d3e
Michael Codner <mhcodner@gmail.com>
2015-09-07 22:07:30 +1000

    changed to HTTP POST instead of GET

--- controllers/contact.php
+++ controllers/contact.php
@@ -10,19 +10,20 @@ class JSON_API_Contact_Controller
     public function send_message()
     {
         global $json_api;
-        $name = $json_api->query->name;
-        $email = $json_api->query->email;
-        $message = $json_api->query->message;
-        if (!$name || !$email || !$message) {
-            $json_api->error("Please fill out all form fields.");
+        nocache_headers();
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        if (empty($name) || empty($email) || empty($message)) {
+            $json_api->error("Please fill out all required form fields (name, email, message).");
         }
         if ($email) {
             sanitize_email($email);
         }
         if (!is_email($email)) {
-            $json_api->error("Please provide a valid email.");
+            $json_api->error("Please provide a valid email address.");
         }
-        $email_headers = 'From: ' . $name . '<' . $email . '>' . "\r\n";
+        $email_headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
         $admin_email = get_option('admin_email');
         wp_mail($admin_email, 'Contact form submission', $message, $email_headers);
         return array(
